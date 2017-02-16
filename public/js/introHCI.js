620d8862c1a89bda8978e31a3b3d554f7f4db4f1
Cathy Liu <cyjliu@gmail.com>
2017-02-16 01:37:28 -0800

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -14,6 +14,8 @@ function initializePage() {
 	$('#colorBtn').click(randomizeColors);
 }
 
+
+
 /*
  * Make an AJAX call to retrieve project details and add it in
  */
@@ -26,8 +28,26 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	
 	console.log("User clicked on project " + idNumber);
+	console.log("/project/" + idNumber);
+
+	$.get("/project/" + idNumber, addProject);
 }
+
+
+function addProject(result){
+	console.log(result);
+
+	var selector = $("#" + "project" + result['id'] + " div.details");
+	var projectHTML ='<a href="#" class="thumbnail">' + 
+	'<img src="' + result['image'] + '" class="img detailsImage">' +
+	'<p>' + result['title'] + '</p>' + 
+	'<p><small>' + result['date'] + '</small></p></a>' + result['summary'];
+
+	selector.html(projectHTML);
+}
+
 
 /*
  * Make an AJAX call to retrieve a color palette for the site
@@ -35,4 +55,19 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+
+	$.get("/palette", addColor);
 }
+
+function addColor(result){
+	console.log(result);
+
+	var colors = result.colors.hex;
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+}
+
+
